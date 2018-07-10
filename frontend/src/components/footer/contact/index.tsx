17de53bcb5af3acc46de26e897fa571fc3c29033
Michael Stockerl <stockerl@unternehmertum.de>
2018-07-10 08:20:34 +0200

    Add more content to footer, split footer into multiple smaller components

---
+++ frontend/src/components/footer/contact/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+class Contact extends React.Component {
+  render() {
+    return (
+      <div className="contact">
+        Questions? <br/>
+        Contact us, we are friendly:<br />
+        <span className="contact__email">hello@dpschool.io</span>
+      </div>
+    )
+  }
+}
+
+export default Contact;
