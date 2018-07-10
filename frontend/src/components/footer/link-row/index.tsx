b065b9434d5296e8331418027f53f293807feae1
Michael Stockerl <stockerl@unternehmertum.de>
2018-07-10 08:57:15 +0200

    Improved layout on desktop

--- frontend/src/components/footer/link-row/index.tsx
+++ frontend/src/components/footer/link-row/index.tsx
@@ -3,13 +3,17 @@ import * as React from 'react';
 class LinkRow extends React.Component {
   render() {
     return (
-      <div className="link_row">
-        <a href="/legal_disclosure">Impressum – Legal Disclosure</a>
-        <a href="/privacy_policy">Privacy Policy</a>
-        <a href="https://www.facebook.com/digitalproductschool"><img className="link_row__social__media" src="/assets/facebook.svg" /></a>
-        <a href="https://twitter.com/dpschool_io"><img className="link_row__social__media" src="/assets/twitter.svg" /></a>
-        <a href="https://www.instagram.com/digitalproductschool/"><img className="link_row__social__media" src="/assets/instagram.svg" /></a>
-        <a href="https://www.youtube.com/channel/UCvmerSnTMl2ZeoLtuKoONJQ/videos"><img className="link_row__social__media" src="/assets/youtube.svg" /></a>
+      <div className="link_row u-content">
+        <div className="link_row__legal">
+          <a href="/legal_disclosure">Impressum – Legal Disclosure</a>
+          <a href="/privacy_policy">Privacy Policy</a>
+        </div>
+        <div className="link_row__social__media">
+          <a href="https://www.youtube.com/channel/UCvmerSnTMl2ZeoLtuKoONJQ/videos"><img className="link_row__social__media__link" src="/assets/youtube.svg" /></a>
+          <a href="https://www.instagram.com/digitalproductschool/"><img className="link_row__social__media__link" src="/assets/instagram.svg" /></a>
+          <a href="https://twitter.com/dpschool_io"><img className="link_row__social__media__link" src="/assets/twitter.svg" /></a>
+          <a href="https://www.facebook.com/digitalproductschool"><img className="link_row__social__media__link" src="/assets/facebook.svg" /></a>
+        </div>
       </div>
     );
   }
