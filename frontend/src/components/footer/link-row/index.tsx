fc1c0e9457c80f74c7136f633b3dd9d8de18f508
Michael Stockerl <stockerl@unternehmertum.de>
2018-07-10 16:20:12 +0200

    Refactored SVG display using React library

--- frontend/src/components/footer/link-row/index.tsx
+++ frontend/src/components/footer/link-row/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import SVG from 'react-inlinesvg'
 
 class LinkRow extends React.Component {
   render() {
@@ -9,10 +10,18 @@ class LinkRow extends React.Component {
           <a href="/privacy_policy">Privacy Policy</a>
         </div>
         <div className="link_row__social__media">
-          <a href="https://www.youtube.com/channel/UCvmerSnTMl2ZeoLtuKoONJQ/videos"><img className="link_row__social__media__link" src="/assets/youtube.svg" /></a>
-          <a href="https://www.instagram.com/digitalproductschool/"><img className="link_row__social__media__link" src="/assets/instagram.svg" /></a>
-          <a href="https://twitter.com/dpschool_io"><img className="link_row__social__media__link" src="/assets/twitter.svg" /></a>
-          <a href="https://www.facebook.com/digitalproductschool"><img className="link_row__social__media__link" src="/assets/facebook.svg" /></a>
+          <a href="https://www.youtube.com/channel/UCvmerSnTMl2ZeoLtuKoONJQ/videos">
+            <SVG src="/assets/youtube.svg" className="link_row__social__media__link"></SVG>
+          </a>
+          <a href="https://www.instagram.com/digitalproductschool/">
+            <SVG className="link_row__social__media__link" src="/assets/instagram.svg"></SVG>
+          </a>
+          <a href="https://twitter.com/dpschool_io">
+            <SVG className="link_row__social__media__link" src="/assets/twitter.svg"></SVG>
+          </a>
+          <a href="https://www.facebook.com/digitalproductschool">
+            <SVG className="link_row__social__media__link" src="/assets/facebook.svg"></SVG>
+          </a>
         </div>
       </div>
     );
