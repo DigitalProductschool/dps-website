d8f940957871d9f2807d3d19bcdfdc5152ce852d
Michael Stockerl <stockerl@unternehmertum.de>
2018-07-16 16:27:29 +0200

    Use u-link class also in What's new component

--- frontend/src/components/whats-new/index.tsx
+++ frontend/src/components/whats-new/index.tsx
@@ -8,7 +8,7 @@ class WhatsNew extends React.Component {
           <h3 className="u-title">WHAT'S NEW</h3>
           <div className="whats-new__content__tiles">
             <div className="item-1">
-              <a href="https://leaks.digitalproductschool.io/design-thinking-a-creative-way-to-foster-innovation-ab9ef97cd884" className="whats-new__content__tile">
+              <a href="https://leaks.digitalproductschool.io/design-thinking-a-creative-way-to-foster-innovation-ab9ef97cd884" className="whats-new__content__tile u-link">
                 <div className="whats-new__content__tile__thumb">
                   <img src="/assets/design-thinking.gif" />
                 </div>
@@ -20,7 +20,7 @@ class WhatsNew extends React.Component {
               </a>
             </div>
             <div className="item-2">
-              <a href="https://leaks.digitalproductschool.io/digital-product-school-dps-86f48a7d4d69" className="whats-new__content__tile">
+              <a href="https://leaks.digitalproductschool.io/digital-product-school-dps-86f48a7d4d69" className="whats-new__content__tile u-link">
                 <div className="whats-new__content__tile__thumb">
                   <img src="/assets/review.jpg" />
                 </div>
@@ -32,7 +32,7 @@ class WhatsNew extends React.Component {
               </a>
             </div>
             <div className="item-3">
-              <a href="https://www.youtube.com/watch?v=pVikgqTpulg" className="whats-new__content__tile">
+              <a href="https://www.youtube.com/watch?v=pVikgqTpulg" className="whats-new__content__tile u-link">
                 <div className="whats-new__content__tile__thumb u-youtube">
                   <iframe src="https://www.youtube.com/embed/pVikgqTpulg?rel=0&amp;showinfo=0"></iframe>
                 </div>
