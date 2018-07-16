2c32c1ef0c5057699bbcf5396bed86123b3b5d50
lustoykov <lustoykov@gmail.com>
2018-07-16 18:49:46 +0200

    Add scroll to top on page transition

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,18 +4,19 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './components/home/index';
 import Legal from './components/legal/index';
 import Privacy from './components/privacy/index';
+import ScrollToTop from './utils/scroll-to-top';
 
 
 export class App extends React.Component {
   render() {
     return (
-      <React.Fragment>
+      <ScrollToTop>
         <Switch>
           <Route path="/index" component={Home} />
           <Route path="/legal-disclosure" component={Legal} />
           <Route path="/privacy-policy" component={Privacy} />
         </Switch>
-      </React.Fragment>
+      </ScrollToTop>
     );
   }
 }
