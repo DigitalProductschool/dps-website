943e8796bb87b5af955afca56029ade9b8f7d5b4
Michael Stockerl <stockerl@unternehmertum.de>
2018-07-16 10:23:54 +0200

    Add content to Privacy policy page

--- frontend/src/components/privacy/index.tsx
+++ frontend/src/components/privacy/index.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
 
+import Nav from '../header/nav/index';
+import PrivacyContent from './privacy-content/index';
+import Footer from '../footer/index';
+
 class Privacy extends React.Component {
   render() {
     return (
-      <p> 
-        This site is very private
-      </p>
+      <React.Fragment>
+        <Nav />
+        <PrivacyContent />
+        <Footer />
+      </React.Fragment>
     );
   }
 }
