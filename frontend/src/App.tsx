213828d22f7dcc7bd2906122cbafdf81fe7bead8
lustoykov <lustoykov@gmail.com>
2018-07-17 09:28:32 +0200

    Revert "Add scroll to top on page transition"
    This reverts commit 2c32c1ef0c5057699bbcf5396bed86123b3b5d50.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,19 +4,18 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './components/home/index';
 import Legal from './components/legal/index';
 import Privacy from './components/privacy/index';
-import ScrollToTop from './utils/scroll-to-top';
 
 
 export class App extends React.Component {
   render() {
     return (
-      <ScrollToTop>
+      <React.Fragment>
         <Switch>
           <Route path="/index" component={Home} />
           <Route path="/legal-disclosure" component={Legal} />
           <Route path="/privacy-policy" component={Privacy} />
         </Switch>
-      </ScrollToTop>
+      </React.Fragment>
     );
   }
 }
