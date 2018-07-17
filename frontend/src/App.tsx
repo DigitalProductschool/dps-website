322792f03d78eccffaba319f6b856cdde15aca86
lustoykov <lustoykov@gmail.com>
2018-07-17 09:44:50 +0200

    Implement scroll to nav

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,18 +4,18 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './components/home/index';
 import Legal from './components/legal/index';
 import Privacy from './components/privacy/index';
-
+import ScrollToNav from './utils/scroll-to-nav';
 
 export class App extends React.Component {
   render() {
     return (
-      <React.Fragment>
+      <ScrollToNav>
         <Switch>
           <Route path="/index" component={Home} />
           <Route path="/legal-disclosure" component={Legal} />
           <Route path="/privacy-policy" component={Privacy} />
         </Switch>
-      </React.Fragment>
+      </ScrollToNav>
     );
   }
 }
