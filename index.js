5e185c14672199cae4551106ecc7f867bc310225
Michael Stockerl <stockerl@unternehmertum.de>
2018-07-18 19:52:49 +0200

    Add content for head tag

--- index.js
+++ index.js
@@ -22,6 +22,13 @@ function htmlTemplate(reactDom, css) {
   <!DOCTYPE html>
   <html>
         <head>
+            <!-- Google Tag Manager -->
+            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5HGJ5CL');</script>
+            <!-- End Google Tag Manager -->
             <meta charset="UTF-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <title>Digital Product School</title>
@@ -31,8 +38,8 @@ function htmlTemplate(reactDom, css) {
             <meta property="og:type" content="website" />
             <meta property="og:url" content="https://digitalproductschool.io" />
             <meta property="og:site_name" content="dp.school" />
-            <meta property="og:image" content="https://digitalproductschool.io/assets/dps.png" />
-            <meta property="og:image:secure_url" content="https://digitalproductschool.io/assets/dps.png" />
+            <meta property="og:image" content="https://digitalproductschool.io/assets/link_dps.png" />
+            <meta property="og:image:secure_url" content="https://digitalproductschool.io/assets/link_dps.png" />
             <meta property="og:image:type" content="image/jpeg" />
             <meta property="og:image:width" content="1200" />
             <meta property="og:image:height" content="630" />
@@ -48,8 +55,13 @@ function htmlTemplate(reactDom, css) {
             <style> ${css} </style>
         </head>
         <body>
-        <div id="app"> ${reactDom} </div>
-        <script type="text/javascript" src="dist/bundle.${buildhash}.js"></script></body>
+          <!-- Google Tag Manager (noscript) -->
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5HGJ5CL"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+          <!-- End Google Tag Manager (noscript) -->
+          <div id="app"> ${reactDom} </div>
+          <script type="text/javascript" src="dist/bundle.${buildhash}.js"></script>
+        </body>
     </html>
   `;
 }
