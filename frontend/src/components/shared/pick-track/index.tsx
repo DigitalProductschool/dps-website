d93fdbdccc35e9e67ba8af2ee7c0c40b5b2c1026
Bastian Rieder <rieder@unternehmertum.de>
2018-11-29 07:33:28 +0100

    Update index.tsx

--- frontend/src/components/shared/pick-track/index.tsx
+++ frontend/src/components/shared/pick-track/index.tsx
@@ -145,7 +145,8 @@ class PickTrack extends React.Component<IPickTrackProps, IPickTrackState> {
       return <p className="pick-track__description">
           There are three batches per year, usually starting in January, May, and September. The application process is ongoing.<br />
           <b>Currently, you can apply for the following batches: <br />
-          - Batch #6: January 7 to March 29, 2019</b>
+          - Batch #6: January 7 to March 29, 2019<br />
+          - Batch #7: May 2 to July 26, 2019</b>
         </p>;
     } else {
       return <p className="pick-track__description">
