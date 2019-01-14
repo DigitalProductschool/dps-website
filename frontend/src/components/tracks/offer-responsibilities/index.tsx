3ad703a43ad88d2a289f1a9fb8ceb021dc5a16a2
lustoykov <lustoykov@gmail.com>
2019-01-14 14:39:36 +0100

    Finish What we Offer & Responsibilities section

--- frontend/src/components/tracks/offer-responsibilities/index.tsx
+++ frontend/src/components/tracks/offer-responsibilities/index.tsx
@@ -16,23 +16,14 @@ export default () => {
           </ul>
         </div>
         <div className="or__responsibilities">
-          <h3>What we offer</h3>
+          <h3 className="or__title">Responsibilities</h3>
           <ul>
-            <li>
-              Working in a diverse team of interaction designers, software developers, AI engineers and fellow product managers who are passionate about building great digital products
-            </li>
-            <li>
-              A unique opportunity to be part of our 3-month training program of the Digital Product School
-            </li>
-            <li>
-              Collaboration with local start-ups, industry leaders, researchers and technology
-            </li>
-            <li>
-              A modern workplace in the rooms of Design Offices Munich, which was designed and build especially for the needs of DPS to work in a creative and inspiring surrounding
-            </li>
-            <li>
-              Grants (750 EUR per month)
-            </li>
+            <ListItem text="Applying suitable user research methodologies and conducting usability tests to deliver valuable insights for other team members" />
+            <ListItem text="Conducting user experiments at any stage of the product development process" />
+            <ListItem text="Defining a product vision and strategy" />
+            <ListItem text="Solving real-world problems through working with several industry partners" />
+            <ListItem text="Collaborate ​with the other product managers, interaction designers, software- and AI engineers in your team on the journey from the problem to the product" />
+            <ListItem text="Defining and measuring KPIs for ensuring your product’s success" />
           </ul>
         </div>
       </div>
