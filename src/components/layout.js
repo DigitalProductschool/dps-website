e65f46527c158ddcdf09d69b397167a1d5858d7c
Ben Rothman <brothman7000@gmail.com>
2019-01-18 00:44:56 +0100

    Testing to see if assets in static folder will run in production with CSS

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
-import './layout.css'
+// import './layout.css'
 import '../styles.css'
 
 const Layout = ({ children }) => (
