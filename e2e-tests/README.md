8795f6b8db219af379d783026fb059c86db85599
Michael Stockerl <stockerl@unternehmertum.de>
2019-01-24 16:35:15 +0100

    Add first end-to-end test to make sure the main page is loaded and Apply now points to the correct form

---
+++ e2e-tests/README.md
@@ -0,0 +1,11 @@
+# End-to-End tests for website
+
+Only write End-to-End tests for important functionality!!
+
+## Install dependencies
+
+`npm install`
+
+## Run tests
+
+`env WEBSITE=https://digitalproductschool.io npm test` e.g. Run all tests against production system
---
+++ e2e-tests/src/apply-now.test.js
@@ -0,0 +1,38 @@
+const {expect} = require('chai');
+const puppeteer = require('puppeteer');
+
+describe('Apply Buttons point to correct page:', function () {
+  this.timeout(7000)
+
+  const website = process.env.WEBSITE
+
+  // Define global variables
+  let browser
+  let page
+
+  before(async function () {
+    browser = await puppeteer.launch()
+    page = await browser.newPage()
+  })
+
+  beforeEach(async function () {
+    page = await browser.newPage()
+    await page.goto(website, {timeout: 0})
+  })
+
+  afterEach(async function () {
+    await page.close()
+  })
+
+  after(async function () {
+    await browser.close()
+  })
+
+
+  it('Main apply button exists and forwards to Typeform', async function () {
+    const expectedHref = 'https://utum.typeform.com/to/qkkFHF?ref=website&entry-point=header'
+
+    const actualHref = await page.evaluate(() => document.querySelector('.header-content__buttons a').getAttribute("href") )
+    expect(actualHref).to.equal(expectedHref)
+  })
+})
