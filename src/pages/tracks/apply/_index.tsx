da6c7819ab8b93ac734c8f0463ab674bea097db7
lustoykov <lustoykov@gmail.com>
2019-02-18 19:40:29 +0100

    Layout improvements

--- src/pages/tracks/apply/_index.tsx
+++ src/pages/tracks/apply/_index.tsx
@@ -12,34 +12,36 @@ export default (props: { track: "pm" | "ai" | "se" | "ixd" }) => {
   let trackName = trackNames[props.track];
 
   return (
-    <div
-      className="u-content"
-      style={{
-        marginTop: "120px",
-      }}
-    >
-    <h3
-      style={{
-        margin: "50px 0",
-      }}
-    > 
-      BE PART OF DPS 
-    </h3>
-    <p> 
-      <b> {`Apply now as a ${trackName} at Digital Product School and take in the experience concentrate in digital product development!`} </b>
-    </p>
-    <br />
-    <p>
-      Currently, you can apply for the following batches: <br />
-      - Batch #7: May 2 to July 26, 2019
-    </p>
-    <div className="tracks__apply-button-wrapper">
-      <a
-        className="u-button u-button__apply u-button--reversed tracks__apply-button" 
-        href={`https://utum.typeform.com/to/qkkFHF?role=${props.track}&ref=website&entry-point=track-page-bottom`}
+    <div className="u-content-wrapper">
+      <div
+        className="u-content"
+        style={{
+          marginTop: "120px",
+        }}
       >
-        Apply as {trackName}
-      </a>
+      <h3
+        style={{
+          margin: "50px 0",
+        }}
+      > 
+        BE PART OF DPS 
+      </h3>
+      <p> 
+        <b> {`Apply now as a ${trackName} at Digital Product School and take in the experience concentrate in digital product development!`} </b>
+      </p>
+      <br />
+      <p>
+        Currently, you can apply for the following batches: <br />
+        - Batch #7: May 2 to July 26, 2019
+      </p>
+      <div className="tracks__apply-button-wrapper">
+        <a
+          className="u-button u-button__apply u-button--reversed tracks__apply-button" 
+          href={`https://utum.typeform.com/to/qkkFHF?role=${props.track}&ref=website&entry-point=track-page-bottom`}
+        >
+          Apply as {trackName}
+        </a>
+      </div>
     </div>
   
   </div>
