8c48f4fa2d19200e638f98200b63d13f4191ca4e
lustoykov <lustoykov@gmail.com>
2019-02-25 13:59:20 +0100

    Change cookie text & button

--- src/pages/shared/cookie-warning/index.tsx
+++ src/pages/shared/cookie-warning/index.tsx
@@ -62,12 +62,13 @@ class CookieWarning extends React.Component<{}, ICookieWarningState> {
           <section className={`cookie-warning u-content-wrapper ${additionalClass}`}>
             <div className="u-content">
               <p className="cookie-warning__description">
-              To help personalize content, measure accesses, and provide a safer experience, we use cookies. 
-              By clicking or navigating the site, you agree to allow our collection of information on 
-              and off UnternehmerTUM through cookies. Learn more, including about available controls: <a href="/privacy-policy#cookies">Cookies Policy.</a>
+                To help personalize content, measure accesses, and provide a safer experience, 
+                we would like to use cookies. Please click on "I agree" to allow us the collection 
+                of information on and off UnternehmerTUM through cookies. Learn more, 
+                including about available controls: <a href="/privacy-policy#cookies">Cookies Policy.</a>
               </p>
               <button className="cookie-warning__button u-button" onClick={() => this.setState({ animateOut: true })}>
-                Close
+                I agree
               </button>
             </div>
           </section>
