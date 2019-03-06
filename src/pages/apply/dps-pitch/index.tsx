be0c54049438222b96ba6774bd7221c08905ca97
BastianRieder <41467952+BastianRieder@users.noreply.github.com>
2019-03-06 09:57:44 +0100

    Deleted Text

--- src/pages/apply/dps-pitch/index.tsx
+++ src/pages/apply/dps-pitch/index.tsx
@@ -5,11 +5,6 @@ class DPSPitch extends React.Component {
     return (
       <section className="dps-pitch u-content-wrapper u-section-margin-top">
         <div className="u-content">
-          <p className="dps-pitch__content dps-pitch__content--bold">That's where Digital Product School comes in!</p>
-          <p className="dps-pitch__content">
-            We will show you how you act in an autonomous and interdisciplinary team and what "agile", "Scrum" and "Design Thinking" and all the other buzzwords mean.
-            You will experience how it is to work with real start-up flair and how to develop digital products that solve real user problems in a lean way.
-          </p>
           <p className="dps-pitch__content dps-pitch__content--small">
             Find out more about <a href="/our-program" className="u-link"><span>our program</span></a> or apply now for one of our roles below!
           </p>
