5c31dc1eae774e6c512b11c5de57abb0566396d4
BastianRieder <41467952+BastianRieder@users.noreply.github.com>
2019-03-06 09:54:25 +0100

    Changed test in first paragraph

--- src/pages/apply/apply-now/index.tsx
+++ src/pages/apply/apply-now/index.tsx
@@ -5,10 +5,10 @@ class ApplyNow extends React.Component {
     return (
       <section className="apply-now u-content-wrapper">
         <div className="apply-now__content u-content">
-          <h2 className="u-title">Apply now and Become Experienced!</h2>
+          <h2 className="u-title">Become an experienced digital product maker!</h2>
           <div className="apply-now__content__description u-grid-4-6">
             <p className="u-text">
-              You can be the best graduate of all time, but when you go to your first job after uni, you're a newbie. Maybe you have heard that modern tech companies nowadays work in "agile teams" and no longer in departments and divisions.<br/> Instead of top-down management, there are flat hierarchies and the teams organize themselves and their projects on their own. <br/> You also might have heard about "agile", "Scrum" and "Design Thinking". But what's that in reality? How does it work in the real world?
+              Digital Product School will show you how you act in an autonomous and interdisciplinary team and what "agile", "Scrum" and "Design Thinking" and all the other buzzwords mean. You will experience how it is to work with real start-up flair and how to develop digital products that solve real user problems in a lean way.
             </p>
             <img alt="apply-now-image" className="apply-now__content__description__image u-image" src="/assets/apply/apply-now-desc.JPG" />
           </div>
