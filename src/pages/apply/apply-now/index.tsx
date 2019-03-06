ba11ae4fa99f1404040fc75bbf454775033151af
BastianRieder <41467952+BastianRieder@users.noreply.github.com>
2019-03-06 14:30:35 +0100

    Insert /

--- src/pages/apply/apply-now/index.tsx
+++ src/pages/apply/apply-now/index.tsx
@@ -9,7 +9,7 @@ class ApplyNow extends React.Component {
           <div className="apply-now__content__description u-grid-4-6">
             <p className="u-text">
               Digital Product School will show you how you act in an autonomous and interdisciplinary team and what "agile", "Scrum" and "Design Thinking" and all the other buzzwords mean. You will experience how it is to work with real start-up flair and how to develop digital products that solve real user problems in a lean way.
-              <br>
+              <br/>
               Find out more about <a href="/our-program" className="u-link"><span>our program</span></a> or apply now for one of our roles below!          
             </p>
             <img alt="apply-now-image" className="apply-now__content__description__image u-image" src="/assets/apply/apply-now-desc.JPG" />
