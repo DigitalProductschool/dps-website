9e64851f002ab95f4fdec14cc3362d2f9975c7df
bhawna333 <bhawna.bs.03@gmail.com>
2019-03-08 10:03:35 +0100

    50x error page layout

---
+++ src/pages/50x.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import Footer from './shared/footer/index';
+import Nav from './shared/header/nav/index';
+import CookieWarning from './shared/cookie-warning';
+
+
+const NotFoundPage = () => (
+  <React.Fragment>
+    <CookieWarning />
+    <Nav addClass={"nav--black"} logo={"/assets/shared/dps-logo-black.svg"} />
+    <section className="not-found u-content-wrapper">
+      <div className="not-found__content u-content">
+        <h2 className="u-title">Service Unavailable :(</h2>
+        <div className="apply-now__content__description">
+          <p className="u-text">
+            Looks like we are having some server issues.
+          </p>
+        </div>
+      </div>
+    </section>
+    <Footer />
+  </React.Fragment>
+)
+
+export default NotFoundPage
