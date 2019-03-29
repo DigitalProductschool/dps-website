cc882f5a3e5f1b9f8e1e0c3620423c0372664f35
Michael Stockerl <stockerl@unternehmertum.de>
2019-03-29 10:13:27 +0100

    Add canonical url to head section

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://digitalproductschool.io`,
+      },
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
