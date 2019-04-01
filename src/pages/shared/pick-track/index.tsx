2b13b4d3eb2c1375250248f28f5cca971778d149
BastianRieder <41467952+BastianRieder@users.noreply.github.com>
2019-04-01 09:43:21 +0200

    Deleted Application Batch #7

--- src/pages/shared/pick-track/index.tsx
+++ src/pages/shared/pick-track/index.tsx
@@ -145,7 +145,6 @@ class PickTrack extends React.Component<IPickTrackProps, IPickTrackState> {
       return <p className="pick-track__description">
           There are three batches per year, usually starting in January, May, and September.<br />
           <b>Currently, you can apply for the following batches: <br />
-          # Batch #7: May 2 to July 26, 2019</b> (Application deadline: March 31, 2019)<b><br />
           # Batch #8: Sept. 10 to Nov. 29, 2019</b> (Application deadline: July 14, 2019)
         </p>;
     } else {
