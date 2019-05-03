2fee76ac5a064c4ca47c45c70755d68228f64d38
BastianRieder <41467952+BastianRieder@users.noreply.github.com>
2019-05-03 17:55:04 +0200

    New Adress ATLAS

--- src/components/shared/footer/about/index.tsx
+++ src/components/shared/footer/about/index.tsx
@@ -12,9 +12,9 @@ class About extends React.Component {
           <a href="https://www.google.de/maps/place/Digital+Product+School/@48.1763748,11.5904089,17z/data=!3m1!4b1!4m5!3m4!1s0x479e744a68f476c1:0x7a08a7452f6d4afe!8m2!3d48.1763748!4d11.5926029?shorturl=1" className="about__link">We are here</a>
           <div className="about__address__content">
             Digital Product School<br />
-            c/o DesignOffices<br />
-            Mies-van-der-Rohe-Str. 8<br />
-            80807 Munich<br />
+            c/o DesignOffices / UnternehmerTUM<br />
+            Rosenheimer Straße 143C<br />
+            81671 Munich<br />
             Germany
           </div>
         </div>
