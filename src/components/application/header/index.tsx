61ca7f308b1d4bf1fe5df779bdbf0b7bf31368bf
bhawna333 <bhawna.bs.03@gmail.com>
2019-10-08 14:49:35 +0200

    batch data from firestore

--- src/components/application/header/index.tsx
+++ src/components/application/header/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { getFirebase } from '../../../firebase/firebase';
 
 import Nav from '../../shared/nav/index';
 
@@ -39,9 +40,65 @@ interface IHeaderProps {
   track: 'se' | 'pm' | 'ixd' | 'ai';
 }
 
+function getBatchDate(batchDate) {
+  let shortMonthName = new Intl.DateTimeFormat('en-US', { month: 'short' })
+    .format;
+  const monthNames = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+  let date = batchDate.toDate();
+  let newdate =
+    monthNames[date.getMonth()] +
+    ' ' +
+    date.getDate() +
+    ', ' +
+    date.getFullYear();
+  return newdate;
+}
+
+function dateCompare(date) {
+  let today = new Date();
+  let givenDate = date.toDate();
+  return givenDate <= today;
+}
+
 class Header extends React.Component<IHeaderProps, {}> {
+  constructor(props: IHeaderProps) {
+    super(props);
+    this.state = { batchDetails: [] };
+  }
+
   render() {
     const { track } = this.props;
+    let displayBatch = this.state.batchDetails.map(
+      function(batch) {
+        if (dateCompare(batch.appStartDate))
+          return (
+            <span>
+              <b>
+                {`#Batch #${batch.batchNumber}: ${getBatchDate(
+                  batch.startDate
+                )} to ${getBatchDate(batch.endDate)} `}
+              </b>
+              {`(Application phase: ${getBatchDate(
+                batch.appStartDate
+              )} to ${getBatchDate(batch.appEndDate)})`}
+              <br />
+            </span>
+          );
+      }.bind(this)
+    );
 
     return (
       <section
@@ -54,12 +111,41 @@ class Header extends React.Component<IHeaderProps, {}> {
           </h1>
           <br />
           <p>Currently you can apply for the following batches:</p>
-          <p>
-            #Batch #9: Jan 7, 2020 to Mar 27, 2020 (Deadline: October 13, 2019)
-          </p>
+          <p>{displayBatch}</p>
         </div>
       </section>
     );
+  }
+
+  componentDidMount() {
+    const firebaseApp = import('@firebase/app');
+    const firebaseDatabase = import('@firebase/firestore');
+    var currentTime = new Date();
+    Promise.all([firebaseApp, firebaseDatabase]).then(([firebase]) => {
+      const database = getFirebase(firebase).firestore();
+      database
+        .collection('batch-details')
+        .where('appEndDate', '>', currentTime)
+        .orderBy('appEndDate')
+        .get()
+        .then(snapshot =>
+          snapshot.forEach(doc =>
+            this.setState(prevState => ({
+              batchDetails: [
+                ...prevState.batchDetails,
+                {
+                  batchID: doc.id,
+                  batchNumber: doc.data().batch,
+                  startDate: doc.data().startDate,
+                  endDate: doc.data().endDate,
+                  appStartDate: doc.data().appStartDate,
+                  appEndDate: doc.data().appEndDate,
+                },
+              ],
+            }))
+          )
+        );
+    });
   }
 }
 
