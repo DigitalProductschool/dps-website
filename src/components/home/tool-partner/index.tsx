31fb23727a73eb05c741287bfe7dd98eecb1ea25
BastianRieder <41467952+BastianRieder@users.noreply.github.com>
2019-10-11 16:23:03 +0200

    New UTUM-Logo and New alt-tags

--- src/components/home/tool-partner/index.tsx
+++ src/components/home/tool-partner/index.tsx
@@ -7,17 +7,17 @@ class ToolPartner extends React.Component {
         <div className="tool-partner__content u-content">
           <h3 className="u-title">TOOL PARTNERS</h3>
           <div className="tool-partner__content__logos">
-            <img alt="tool-partner-eyequant" className="tool-partner__content__logo" src="/assets/home/tool-partner/eyequant.jpg" />
-            <img alt="tool-partner-invision" className="tool-partner__content__logo" src="/assets/home/tool-partner/invision.png" />
-            <img alt="tool-partner-uxpin" className="tool-partner__content__logo" src="/assets/home/tool-partner/uxpin.png" />
-            <img alt="tool-partner-github" className="tool-partner__content__logo" src="/assets/home/tool-partner/github.png" />
+            <img alt="Eyequant" className="tool-partner__content__logo" src="/assets/home/tool-partner/eyequant.jpg" />
+            <img alt="Invision" className="tool-partner__content__logo" src="/assets/home/tool-partner/invision.png" />
+            <img alt="UXPIN" className="tool-partner__content__logo" src="/assets/home/tool-partner/uxpin.png" />
+            <img alt="github" className="tool-partner__content__logo" src="/assets/home/tool-partner/github.png" />
             <div>
               <p> DPS is a program by </p>
               <p> funded by </p>
               <p> in cooperation with </p>
-              <img alt="tool-partner-tum.png" className="tool-partner__content__logo" src="/assets/home/tool-partner/tum.png" />
-              <img alt="tool-partner-staatsministerium-logo" className="tool-partner__content__logo" src="/assets/home/tool-partner/staatsministerium-logo.png" />
-              <img alt="tool-partner-tum.png" className="tool-partner__content__logo" src="/assets/home/tool-partner/tum.png" />
+              <img alt="UnternehmerTUM" className="tool-partner__content__logo" src="/assets/home/tool-partner/UnternehmerTUM.jpg" />
+              <img alt="Bavarian Ministry of Economic Affairs, Regional Development and Energy" className="tool-partner__content__logo" src="/assets/home/tool-partner/staatsministerium-logo.png" />
+              <img alt="Technical University Munich" className="tool-partner__content__logo" src="/assets/home/tool-partner/tum.png" />
             </div>
           </div>
         </div>
