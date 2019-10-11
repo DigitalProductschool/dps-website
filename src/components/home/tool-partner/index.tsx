b80b824a502f0268e5cf2cf31e96a3f7baa3c828
BastianRieder <41467952+BastianRieder@users.noreply.github.com>
2019-10-11 16:28:09 +0200

    Insert Title-Tags at Images

--- src/components/home/tool-partner/index.tsx
+++ src/components/home/tool-partner/index.tsx
@@ -7,17 +7,17 @@ class ToolPartner extends React.Component {
         <div className="tool-partner__content u-content">
           <h3 className="u-title">TOOL PARTNERS</h3>
           <div className="tool-partner__content__logos">
-            <img alt="Eyequant" className="tool-partner__content__logo" src="/assets/home/tool-partner/eyequant.jpg" />
-            <img alt="Invision" className="tool-partner__content__logo" src="/assets/home/tool-partner/invision.png" />
-            <img alt="UXPIN" className="tool-partner__content__logo" src="/assets/home/tool-partner/uxpin.png" />
-            <img alt="github" className="tool-partner__content__logo" src="/assets/home/tool-partner/github.png" />
+            <img alt="Eyequant" title="Eyequant, tool partner of Digital Product School" className="tool-partner__content__logo" src="/assets/home/tool-partner/eyequant.jpg" />
+            <img alt="Invision" title="Invision, tool partner of Digital Product School" className="tool-partner__content__logo" src="/assets/home/tool-partner/invision.png" />
+            <img alt="UXPIN" title="UXPIN, tool partner of Digital Product School" className="tool-partner__content__logo" src="/assets/home/tool-partner/uxpin.png" />
+            <img alt="github" title="github, tool partner of Digital Product School" className="tool-partner__content__logo" src="/assets/home/tool-partner/github.png" />
             <div>
               <p> DPS is a program by </p>
               <p> funded by </p>
               <p> in cooperation with </p>
-              <img alt="UnternehmerTUM" className="tool-partner__content__logo" src="/assets/home/tool-partner/UnternehmerTUM.jpg" />
-              <img alt="Bavarian Ministry of Economic Affairs, Regional Development and Energy" className="tool-partner__content__logo" src="/assets/home/tool-partner/staatsministerium-logo.png" />
-              <img alt="Technical University Munich" className="tool-partner__content__logo" src="/assets/home/tool-partner/tum.png" />
+              <img alt="UnternehmerTUM" title="Digital Product School is a program by UnternehmerTUM" className="tool-partner__content__logo" src="/assets/home/tool-partner/UnternehmerTUM.jpg" />
+              <img alt="Bavarian Ministry of Economic Affairs, Regional Development and Energy" title="Digital Product School is funded by the Bavarian Ministry of Economic Affairs, Regional Development and Energy" className="tool-partner__content__logo" src="/assets/home/tool-partner/staatsministerium-logo.png" />
+              <img alt="Technical University Munich" title="Digital Product School takes place in kooperation with the Technical University Munich (TUM)" className="tool-partner__content__logo" src="/assets/home/tool-partner/tum.png" />
             </div>
           </div>
         </div>
