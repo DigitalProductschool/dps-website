2a651bbfb96169caa1fead8f9deb50c12dab7f9c
lustoykov <lustoykov@gmail.com>
2019-11-05 14:06:16 +0100

    Import Link

--- src/components/home/diverse-teams/index.tsx
+++ src/components/home/diverse-teams/index.tsx
@@ -1,39 +1,32 @@
 import * as React from 'react';
+import { Link } from 'gatsby';
 
 class DiverseTeams extends React.Component {
   render() {
     return (
       <div className="diverse-teams u-content-wrapper">
-        <h3 className="diverse-teams__title u-content">
-          Diverse Teams
-        </h3>
+        <h3 className="diverse-teams__title u-content">Diverse Teams</h3>
         <div className="diverse-teams__content u-content">
           <p className="diverse-teams__content__description">
-            We believe that great products are created by diverse and crossfunctional teams. At the Digital Product School,
-            <Link
-             to="/apply/interaction-design"
-             className="u-link"           >
-             <span> interaction designers </span>
-           </Link>
-             ,
-           <Link
-             to="/apply/software-engineering"
-             className="u-link"           >
-             <span> software engineers </span>
-           </Link>
+            We believe that great products are created by diverse and
+            crossfunctional teams. At the Digital Product School,
+            <Link to="/apply/interaction-design" className="u-link">
+              <span> interaction designers </span>
+            </Link>
             ,
-           <Link
-             to="/apply/artificial-intelligence"
-             className="u-link"           >
-             <span> data scientists </span>
-           </Link>
+            <Link to="/apply/software-engineering" className="u-link">
+              <span> software engineers </span>
+            </Link>
+            ,
+            <Link to="/apply/artificial-intelligence" className="u-link">
+              <span> data scientists </span>
+            </Link>
             , and
-           <Link
-             to="/apply/product-management"
-             className="u-link"           >
-             <span> product managers </span>
-           </Link>
-            from all over the world are working together across disciplines to create great products in a lean and agile way.
+            <Link to="/apply/product-management" className="u-link">
+              <span> product managers </span>
+            </Link>
+            from all over the world are working together across disciplines to
+            create great products in a lean and agile way.
           </p>
           <div className="diverse-teams__content__image" />
         </div>
