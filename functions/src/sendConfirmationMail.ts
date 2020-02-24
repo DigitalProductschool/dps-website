3f8a03f0cccf18dd05931c6c1992647219c82ba9
bsinoimeri <b.sinoimeri@hotmail.com>
2020-02-24 13:00:05 +0100

    Changes in the confirmation email

--- functions/src/sendConfirmationMail.ts
+++ functions/src/sendConfirmationMail.ts
@@ -41,31 +41,24 @@ exports.handler = async function(snap: any) {
     from: `${APP_NAME} <application@dpschool.io>`,
     to: `${email}`,
     subject: `Thanks for applying at ${APP_NAME}!`,
-    html: `Hi ${name}, </br></br>
-
-We received your application. Thanks for wanting to be a part of the Digital Product School! </br></br>
-
-We will start to evaluate the applications about 12 weeks before the next batch starts and get in touch with you. If you and your skills fit our needs we will invite you to an interview. Latest four weeks before the batch starts you will know if you are in the next round. </br></br>
-
-If you have any questions, just email us at hello@dpschool.io and stay in touch with us on social media: </br></br>
-
-<a href="https://www.instagram.com/digitalproductschool/"># Instagram </a></br>
-<a href="https://www.facebook.com/digitalproductschool/"># Facebook </a></br>
-<a href="https://www.linkedin.com/company/digital-product-school/"># LinkedIn </a></br>
-<a href="https://twitter.com/dpschool_io"># Twitter </a></br>
-<a href="https://twitter.com/dpschool_io"># Medium  </a></br></br>
-
-Cheers,</br>
-Michael </br></br>
-
-Digital Product School is a project by </br>
-UnternehmerTUM GmbH </br>
-Lichtenbergstr. 6 </br>
-85748 Garching bei München </br>
-+49891894691232 </br></br>
-
-www.digitalproductschool.io </br>
-hello@dpschool.io`,
+    html: `Hi ${name}, <br><br>
+    We received your application. Thanks for wanting to be a part of the Digital Product School! <br><br>
+    We will start to evaluate the applications about 12 weeks before the next batch starts and get in touch with you. If you and your skills fit our needs we will invite you to an interview. Latest four weeks before the batch starts you will know if you are in the next round. <br><br>
+    If you have any questions, just email us at hello@dpschool.io and stay in touch with us on social media: <br><br>
+    <a href="https://www.instagram.com/digitalproductschool/"># Instagram </a><br>
+    <a href="https://www.facebook.com/digitalproductschool/"># Facebook </a><br>
+    <a href="https://www.linkedin.com/company/digital-product-school/"># LinkedIn </a><br>
+    <a href="https://twitter.com/dpschool_io"># Twitter </a><br>
+    <a href="https://twitter.com/dpschool_io"># Medium  </a><br><br>
+    Cheers,<br>
+    Michael <br><br>
+    Digital Product School is a project by <br>
+    UnternehmerTUM GmbH <br>
+    Lichtenbergstr. 6 <br>
+    85748 Garching bei München <br>
+    +49891894691232 <br><br>
+    www.digitalproductschool.io <br>
+    hello@dpschool.io`,
   };
 
   smtpTransport.sendMail(mailOptions, (error: any, info: any) => {
