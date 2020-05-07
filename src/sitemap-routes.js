cb6f79192c5b3292a9acb3e34fa86f711a2b8bdb
bsinoimeri <b.sinoimeri@hotmail.com>
2020-05-07 22:09:28 +0200

    Added the sitemap

---
+++ src/sitemap-routes.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Route } from 'react-router';
+
+export default (
+  <Route>
+    <Route path="/" />
+    <Route path="/apply/" />
+    <Route path="/faq/" />
+    <Route path="/application/software-engineering/" />
+    <Route path="/application/artificial-intelligence/" />
+    <Route path="/application/interaction-design/" />
+    <Route path="/application/product-management/" />
+    <Route path="/team/" />
+    <Route path="/legal-disclosure/" />
+    <Route path="/our-program/" />
+    <Route path="/privacy-policy/" />
+    <Route path="/tracks/artificial-intelligence/" />
+    <Route path="/tracks/interaction-design/" />
+    <Route path="/tracks/product-management/" />
+    <Route path="/tracks/software-engineering/" />
+    <Route path="/404/" />
+    <Route path="/50x/" />
+    <Route path="/home/" />
+    <Route path="/404.html" />
+  </Route>
+);
