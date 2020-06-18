16f6ff251b53c9e3bedf0d2dc7cd70cd8dec3cbe
bsinoimeri <b.sinoimeri@hotmail.com>
2020-06-18 10:54:57 +0200

    Added Medium

---
+++ src/components/home/whats-new/medium/index.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+
+class Medium extends React.Component {
+  state = {
+    author: '',
+    thumbnail: '',
+    link: '',
+    title: '',
+  };
+
+  componentDidMount = () => {
+    fetch(
+      'https://api.rss2json.com/v1/api.json?rss_url=https://leaks.digitalproductschool.io/feed'
+    )
+      .then(res => res.json())
+      .then(data => {
+        const res = data.items[0];
+        const author = res.author;
+        const link = res.link;
+        const thumbnail = res.thumbnail;
+        const title = res.title;
+        this.setState({
+          author: author,
+          link: link,
+          thumbnail: thumbnail,
+          title: title,
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    return (
+      <a
+        href={this.state.link}
+        target="_blank"
+        className="whats-new__content__tile u-link"
+      >
+        <div
+          className="whats-new__content__tile__thumb"
+          style={{ backgroundImage: `url(${this.state.thumbnail})` }}
+        ></div>
+        <article>
+          <h4>{this.state.title}</h4>
+
+          <p>by {this.state.author}</p>
+          <span>Read on Medium ...</span>
+        </article>
+      </a>
+    );
+  }
+}
+
+export default Medium;
