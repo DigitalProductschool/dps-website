0c3f7459d317dd5285245d2a29883888251e2b5c
bsinoimeri <b.sinoimeri@hotmail.com>
2020-10-20 15:12:01 +0200

    Remove our program from Home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,15 +5,13 @@ import Pitch from '../../components/home/pitch/index';
 import DiverseTeams from '../../components/home/diverse-teams';
 import ExperienceMatter from '../../components/home/experience-matters';
 import UserFirst from '../../components/home/user-first';
-import OurProgram from '../../components/home/our-program';
 import Adac from '../../components/home/adac';
-import ToolPartner from '../../components/home/tool-partner'
+import ToolPartner from '../../components/home/tool-partner';
 import IndustryPartners from '../../components/home/industry-partners';
 import WhatsNew from '../../components/home/whats-new';
 import PickTrack from '../../components/shared/pick-track';
 import Footer from '../../components/shared/footer/index';
 import CookieWarning from '../../components/shared/cookie-warning';
-
 
 class Home extends React.Component {
   private forwardedRef: React.RefObject<HTMLDivElement> = React.createRef();
@@ -21,12 +19,18 @@ class Home extends React.Component {
     return (
       <React.Fragment>
         <CookieWarning />
-        <Header onContentButtonClick={() => this.forwardedRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' })} />
+        <Header
+          onContentButtonClick={() =>
+            this.forwardedRef.current.scrollIntoView({
+              behavior: 'smooth',
+              block: 'start',
+            })
+          }
+        />
         <Pitch forwardedRef={this.forwardedRef} />
         <DiverseTeams />
         <ExperienceMatter />
         <UserFirst />
-        <OurProgram />
         <IndustryPartners />
         <PickTrack isApplyNowVersion={false} />
         <WhatsNew />
@@ -34,7 +38,7 @@ class Home extends React.Component {
         <ToolPartner />
         <Footer />
       </React.Fragment>
-    )
+    );
   }
 }
 
