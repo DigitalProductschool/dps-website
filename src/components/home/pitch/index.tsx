81b1b42106f01327bd662062148860adb57ea5ff
Bastian Rieder <41467952+BastianRieder@users.noreply.github.com>
2020-11-17 11:34:46 +0100

    Corrected Typo

--- src/components/home/pitch/index.tsx
+++ src/components/home/pitch/index.tsx
@@ -12,7 +12,7 @@ class Pitch extends React.Component<IPitchProps, {}> {
       <div className="pitch u-content-wrapper" ref={forwardedRef}> 
         <h3 className="u-content pitch__content">
           Build a digital product that users really love. Work in a cross-functional product team. Gain professional experience.<br /> 
-          Be part of our great community of of digital product makers!
+          Be part of our great community of digital product makers!
         </h3>
       </div>
     );
