6f09b61a81a67c493ac96740f956a428a52e9d11
Bastian Rieder <41467952+BastianRieder@users.noreply.github.com>
2020-11-17 11:39:38 +0100

    corona => covid-19

--- src/components/home/remote-collaboration/index.tsx
+++ src/components/home/remote-collaboration/index.tsx
@@ -10,7 +10,7 @@ class RemoteCollaboration extends React.Component {
             <p className="remote-collaboration__description">
               Some months ago we were convinced that real teamwork and innovative digital product development only can happen if
               people from different backgrounds and a big pile of post-its come
-              together in a small team space. But the corona pandemic and all
+              together in a small team space. But the COVID-19 pandemic and all
               restrictions associated with it proved us wrong: it also works
               remotely! With the right tools, a creative and open mindset, and
               also some self-discipline you can be a high-end product team in a
