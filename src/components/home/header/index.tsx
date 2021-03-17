61d303d42a60f1defd9a1f3e5b5a8e8fef010e51
bsinoimeri <b.sinoimeri@hotmail.com>
2021-03-17 14:32:27 +0100

    Solve the image for query Photo

--- src/components/home/header/index.tsx
+++ src/components/home/header/index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 
 import Nav from '../../shared/nav/index';
-import Cover from './cover/index';
 import Header from '../../shared/header/section';
 import HeaderContentInside from '../../shared/header/content-inside-header';
 
@@ -15,19 +14,38 @@ class HomeHeader extends React.Component<IHeaderProps, {}> {
       <React.Fragment>
         <section className="header u-content-wrapper">
           <Nav />
-          <Header
-            image="/assets/home/header/header.jpg"
-            headerType={this.props.headerType}
-          >
-            Master your project. Become
-            <span className="u-outline"> experienced.</span>
-            <HeaderContentInside
-              description="The Digital Product School Munich is where interdisciplinary teams learn how to build digital products that solve real-world problems."
-              buttonText="Apply"
-              buttonUrl="/apply"
-              target="_self"
-            />
-          </Header>
+          {this.props.headerType == 'Photo' ? (
+            <Header image="/assets/home/header/header.jpg">
+              Master your project. Become
+              <span className="u-outline"> experienced.</span>
+              <HeaderContentInside
+                description="The Digital Product School Munich is where interdisciplinary teams learn how to build digital products that solve real-world problems."
+                buttonText="Apply"
+                buttonUrl="/apply"
+                target="_self"
+              />
+            </Header>
+          ) : (
+            <div className="u-content header-content">
+              <video autoPlay muted loop className="cover__video">
+                <source
+                  src="/assets/home/header/headervideo-1680-758.mp4"
+                  type="video/mp4"
+                />
+              </video>
+              <h1 className="header-section__content__title">
+                {' '}
+                Master your project. Become
+                <span className="u-outline"> experienced.</span>
+              </h1>
+              <HeaderContentInside
+                description="The Digital Product School Munich is where interdisciplinary teams learn how to build digital products that solve real-world problems."
+                buttonText="Apply"
+                buttonUrl="/apply"
+                target="_self"
+              />
+            </div>
+          )}
         </section>
       </React.Fragment>
     );
--- src/components/shared/header/section.tsx
+++ src/components/shared/header/section.tsx
@@ -3,38 +3,21 @@ import * as React from 'react';
 interface HeaderProps {
   overlayColor?: string;
   opacity?: string;
-  image: string;
+  image?: string;
   children: React.ReactNode;
-  headerType?: string;
 }
 
 export default function Header(props: HeaderProps) {
   const opacity = props.opacity || '';
   let overlayColor = props.overlayColor || 'rgba(0, 0, 0, 0.56)';
-  let image = props.image;
-  let video;
-
-  if (props.headerType && props.headerType != 'Photo') {
-    video = (
-      <video autoPlay muted loop className="cover__video">
-        <source
-          src="/assets/home/header/headervideo-1680-758.mp4"
-          type="video/mp4"
-        />
-      </video>
-    );
-    image = '';
-    overlayColor = '';
-  }
 
   return (
     <section
       className="header-section u-content-wrapper"
       style={{
-        backgroundImage: 'url(' + image + ')',
+        backgroundImage: 'url(' + props.image + ')',
       }}
     >
-      {video}
       <div className="u-content header-content">
         <div
           style={{
