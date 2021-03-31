3b4f1fbea2f83f4e20fb7b63097283758e228bef
bsinoimeri <b.sinoimeri@hotmail.com>
2021-04-01 00:04:03 +0200

    All the data of Mobility & Healthcare

---
+++ src/components/challenges/data/healthcare.ts
@@ -0,0 +1,144 @@
+export const header = {
+  title: ' The healthcare sector is becoming digital',
+  description:
+    'How might we streamline physicians’ work, optimize systems, improve patient outcomes, reduce human error, and lower costs with new digital products? ',
+  button: 'Find your solution',
+  url: 'https://utum.typeform.com/to/OPGBwNkj',
+  image: '/assets/challenges/healthcare/header.jpg',
+};
+
+export const firststatus = {
+  title: 'Tech-trends and shifting consumer behavior',
+  description1:
+    'While talking about trends in the health industry you may hear about blockchain; 3D-printing; telemedicine; diagnostic artificial intelligence; nanotechnology; predictive healthcare and many more buzzwords. It’s obvious: all fields are massively powered by digitalization. ',
+  description2:
+    'Despite the rapid development of these technologies, the fields of application are still heavily restricted by regulation. Instead, the key trends these days are determined by shifts in consumer behavior. By following and shaping these behaviors, the potential for new business is unlocked and becomes accessible.',
+  description3:
+    'At Digital Product School, teams take a strictly human-centered approach to develop new products and succeed, armed with the right technical expertise and resources to leverage the power of these rising technologies and data.',
+  url: '',
+};
+
+export const partnerbenefits = {
+  title: 'Partner benefits',
+  description1:
+    'At DPS, we empower the next generation of Human-centered digital product makers. We build digital products to solve real user problems and we create new business opportunities.',
+  description2:
+    'As a partner of the Digital Product School, you will be matched with your own cross-functional product team who will work on your challenge for three months and develop an innovative digital product tailored to your needs.',
+  benefit1:
+    'A cross-functional team will work on your challenge for three months',
+  benefit2: 'An innovative digital product that solves your specific challenge',
+  benefit3: 'Access to high-class international talents',
+  benefit4: 'Access to a rich tech-ecosystem',
+};
+
+export const firstcta = {
+  title: 'You are looking for a digital product or business model?',
+  button: 'Contact Us Now',
+  url: 'https://utum.typeform.com/to/OPGBwNkj',
+};
+
+export const successstories = [
+  {
+    image: '/assets/challenges/healthcare/success-stories/healthinprogress.jpg',
+    teamName: 'Batch#10: Team Health in Progress',
+    hmw:
+      'How might we provide patients with knee/hip issues with support throughout their whole treatment?',
+    solution:
+      'An app called “Arthro-Buddy” that allows patients to easily log and track the efficacy of different treatments. An algorithm evaluates the impact of these treatments and helps patients to recover faster and to get rid of restrictions caused by osteoarthritis.',
+  },
+  {
+    image: '/assets/challenges/healthcare/success-stories/beatfine.jpg',
+    teamName: 'Batch#10: Team 90 Degrees',
+    hmw:
+      'How Might we enable patients to know and track the conditions of their cardiovascular system on a regular basis to prevent irreversible damages?',
+    solution:
+      '“Beatfine”, a mobile app that guides people after surgery and assists them when picking exercises and activities to provide peace of mind. The core feature utilizes a diary for smart journaling to track the rehabilitation process for both doctors and patients.',
+    more: 'Watch their final presentation on Youtube',
+    url: 'https://www.youtube.com/watch?v=7RMcPfLjeYc',
+  },
+  {
+    image: '/assets/challenges/healthcare/success-stories/mangoseeds.jpg',
+    teamName: 'Batch#11: Team MangoSeeds',
+    hmw:
+      "How might we support surgeon's planning procedure by ensuring that the surgeons are presented with just the right amount of information in order to shorten the planning while still allowing them to stay in control.",
+    solution:
+      'InVue, an AI-powered planning software built to provide spine surgeons with the tools they need to prepare, plan and execute fast and reliably, independent of hospital infrastructure.',
+    more: 'InVue is now part of the start-up Mango Medical ',
+    url: 'https://www.mangomedical.de/',
+  },
+];
+
+export const process = {
+  title: 'The Road to Success',
+  description1:
+    'The process of Human-centered product development consists of three parts: <b>GATHERING</b> data and input about each challenge; <b>DISCOVERING</b> a user problem and creating a solution for it and the <b>ROLLOUT</b> of the product and the business model.',
+  description2:
+    'In the <b>GATHERING PHASE</b> a company’s challenge is discussed and framed into a “wouldn’t-it-be-great” question. Then, we form the team of talents who will solve that challenge.',
+  description3:
+    'That’s the starting point for the core success factor: the <b>DISCOVERY PHASE</b>. For three months, autonomous and cross-functional teams of Product Managers, Agile Coaches, Interaction Designer, Software Engineers, and AI Engineers will build an innovative digital product that solves a real user problem. The process is powered by the best-in-class tools and methods from Design Thinking, Lean Startup and agile product development.',
+  description4:
+    'In the <b>ROLLOUT PHASE</b>, we support our partners in developing the product and business model and in taking it to market.',
+  image: '/assets/challenges/mobility/road.png',
+};
+
+export const mucl = {
+  title: 'A unique location: The Munich Urban Colab',
+  description1:
+    'The Digital Product School is located in the most creative, dynamic and entrepreneurial nerve center for innovation: The Munich Urban Colab. There, talents, promising entrepreneurs, start-ups, corporate innovators, scientists, and creatives from the most diverse sectors and disciplines work together under one roof.',
+  description2:
+    'It provides the most modern infrastructure to explore new technologies and use them to build innovative products.',
+  image: '/assets/challenges/mucl.jpg',
+};
+
+export const whydps = [
+  {
+    title: 'Experience',
+    description: '12 Batches, 400 Talents 100 Teams',
+    icon: '/assets/challenges/mobility/pillars/Digital.svg',
+  },
+  {
+    title: 'Expertise',
+    description: 'A core team of experts in their subjects',
+    icon: '/assets/challenges/mobility/pillars/Team.svg',
+  },
+  {
+    title: 'Ecosystem',
+    description: 'Part of Europe’s biggest entrepreneurship center',
+    icon: '/assets/challenges/mobility/pillars/Frame.svg',
+  },
+  {
+    title: 'Independence',
+    description: 'UnternehmerTUM GmbH is a non-profit organization',
+    icon: '/assets/challenges/mobility/pillars/Group.svg',
+  },
+  {
+    title: 'Location',
+    description:
+      'The Munich Urban Colab as the palace where future mobility concepts are developed',
+    icon: '/assets/challenges/mobility/pillars/Location.svg',
+  },
+  {
+    title: 'Mission',
+    description:
+      'It’s our mission and passion to shape the world by solving the challenges of the future',
+    icon: '/assets/challenges/mobility/pillars/Goal.svg',
+  },
+  {
+    title: 'Partners',
+    description:
+      'Connection to the mobility sector and beyond to enrich your innovative business model',
+    icon: '/assets/challenges/mobility/pillars/Network.svg',
+  },
+  {
+    title: 'Talents',
+    description:
+      '500 applications per batch. The best students and graduates want to join DPS.',
+    icon: '/assets/challenges/mobility/pillars/Education.svg',
+  },
+];
+
+export const secondcta = {
+  title: 'Let’s tackle and solve your mobility-related challenge!',
+  button: 'Contact Us Now',
+  url: 'https://utum.typeform.com/to/OPGBwNkj',
+};
