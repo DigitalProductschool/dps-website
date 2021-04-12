3a95d967abd172c6f1a620badbb2c0f3f4ea2d55
bsinoimeri <b.sinoimeri@hotmail.com>
2021-04-12 11:51:05 +0200

    New section - Pick your Track

--- src/components/shared/pick-track/index.tsx
+++ src/components/shared/pick-track/index.tsx
@@ -6,7 +6,7 @@ interface IPickTrackProps {
 }
 
 interface IPickTrackState {
-  active: 'pm' | 'se' | 'ai' | 'ixd' | null;
+  active: 'pm' | 'se' | 'ai' | 'ixd' | 'pmc' | 'ac' | null;
 }
 
 class PickTrack extends React.Component<IPickTrackProps, IPickTrackState> {
@@ -28,7 +28,6 @@ class PickTrack extends React.Component<IPickTrackProps, IPickTrackState> {
               <a href="/tracks/product-management/">
                 <div className="pick-tracks__track__image-wrapper">
                   <div className="pick-tracks__track__image pick-tracks__track__image--pm" />
-
                   <img
                     alt="pm-icon"
                     className="pick-tracks__track__icon"
@@ -54,7 +53,7 @@ class PickTrack extends React.Component<IPickTrackProps, IPickTrackState> {
                 onMouseOver={() => this.setState({ active: 'pm' })}
                 onMouseOut={() => this.setState({ active: null })}
               >
-                Apply as PM
+                Apply
               </a>
             </li>
 
@@ -62,7 +61,6 @@ class PickTrack extends React.Component<IPickTrackProps, IPickTrackState> {
               <a href="/tracks/interaction-design/">
                 <div className="pick-tracks__track__image-wrapper">
                   <div className="pick-tracks__track__image pick-tracks__track__image--ixd" />
-
                   <img
                     alt="ixd-icon"
                     className="pick-tracks__track__icon"
@@ -89,14 +87,14 @@ class PickTrack extends React.Component<IPickTrackProps, IPickTrackState> {
                 onMouseOver={() => this.setState({ active: 'ixd' })}
                 onMouseOut={() => this.setState({ active: null })}
               >
-                Apply as IxD
+                Apply
               </a>
             </li>
+
             <li key="se" className="pick-track__track">
               <a href="/tracks/software-engineering/">
                 <div className="pick-tracks__track__image-wrapper">
                   <div className="pick-tracks__track__image pick-tracks__track__image--se" />
-
                   <img
                     alt="se-icon"
                     className="pick-tracks__track__icon"
@@ -123,14 +121,14 @@ class PickTrack extends React.Component<IPickTrackProps, IPickTrackState> {
                 onMouseOver={() => this.setState({ active: 'se' })}
                 onMouseOut={() => this.setState({ active: null })}
               >
-                Apply as SE
+                Apply
               </a>
             </li>
+
             <li key="ai" className="pick-track__track">
               <a href="/tracks/artificial-intelligence">
                 <div className="pick-tracks__track__image-wrapper">
                   <div className="pick-tracks__track__image pick-tracks__track__image--ai" />
-
                   <img
                     alt="ai-icon"
                     className="pick-tracks__track__icon"
@@ -157,7 +155,82 @@ class PickTrack extends React.Component<IPickTrackProps, IPickTrackState> {
                 onMouseOver={() => this.setState({ active: 'ai' })}
                 onMouseOut={() => this.setState({ active: null })}
               >
-                Apply as AI
+                Apply
+              </a>
+            </li>
+
+            <li key="ac" className="pick-track__track">
+              <a href="/tracks/agile-coach/">
+                <div className="pick-tracks__track__image-wrapper">
+                  <div className="pick-tracks__track__image pick-tracks__track__image--ac" />
+                  <img
+                    alt="ac-icon"
+                    className="pick-tracks__track__icon"
+                    src="/assets/shared/pick-track/ac-icon.png"
+                    style={active === 'ac' ? { opacity: 0.1 } : {}}
+                  />
+                </div>
+              </a>
+              <p className="pick-track__track__description">
+                <a className="u-link" href="/tracks/agile-coach/">
+                  <span> Agile Team Coach </span>
+                </a>{' '}
+                assist cross-functional, diverse teams and help them to become
+                fast-moving and fast learning teams that frequently deliver
+                results.
+                <br />
+                <a className="u-link" href="/tracks/agile-coach/">
+                  <span>More ...</span>
+                </a>{' '}
+              </p>
+              <a
+                className="pick-track__track__button pick-track__track__button--ac u-button u-button--reversed"
+                href="/application/agile-coach/"
+                onMouseOver={() => this.setState({ active: 'ac' })}
+                onMouseOut={() => this.setState({ active: null })}
+              >
+                Apply
+              </a>
+            </li>
+
+            <li key="pmc" className="pick-track__track">
+              <a href="/tracks/product-marketing-communications-manager/">
+                <div className="pick-tracks__track__image-wrapper">
+                  <div className="pick-tracks__track__image pick-tracks__track__image--pmc" />
+
+                  <img
+                    alt="ai-icon"
+                    className="pick-tracks__track__icon"
+                    src="/assets/shared/pick-track/pmc-icon.png"
+                    style={active === 'pmc' ? { opacity: 0.1 } : {}}
+                  />
+                </div>
+              </a>
+              <p className="pick-track__track__description">
+                <a
+                  className="u-link"
+                  href="/tracks/product-marketing-communications-manager"
+                >
+                  <span>Product Marketing &amp; Communication Managers </span>
+                </a>{' '}
+                are responsibility is to communicate the product’s value to the
+                market. They know the product, the users, and how to arouse
+                attention for it.
+                <br />
+                <a
+                  className="u-link"
+                  href="/tracks/product-marketing-communications-manager/"
+                >
+                  <span>More ...</span>
+                </a>{' '}
+              </p>
+              <a
+                className="pick-track__track__button pick-track__track__button--pmc u-button u-button--reversed"
+                href="/application/product-marketing-communications-manager/"
+                onMouseOver={() => this.setState({ active: 'pmc' })}
+                onMouseOut={() => this.setState({ active: null })}
+              >
+                Apply
               </a>
             </li>
           </ul>
@@ -190,10 +263,14 @@ class PickTrack extends React.Component<IPickTrackProps, IPickTrackState> {
     } else {
       return (
         <p className="pick-track__description">
-          Our teams are »cross-functional«. This means that product managers,
-          interaction designers, software engineers and artificial intelligence
-          engineers work together to find out what the user needs and to build
-          the product.
+          At the Digital Product School, we have »cross-functional« product
+          teams: Product Managers, Interaction Designers, Software Engineers,
+          and Artificial Intelligence Engineers work together in a team, explore
+          and understand user problems and solve them by building a digital
+          product. The Agile Team Coaches support them to perform as a team. The
+          Product Marketing and Communications Managers work in their own teams
+          on specific communication challenges. <br />
+          What's your preferred role in these teams? Choose now!
         </p>
       );
     }
