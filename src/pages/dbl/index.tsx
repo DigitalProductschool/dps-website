7105ba7965848b6379efa041ca7546c3cab6b0a5
bsinoimeri <b.sinoimeri@hotmail.com>
2021-10-20 14:28:26 +0200

    Redirect dbl

---
+++ src/pages/dbl/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+class DBL extends React.Component {
+  componentDidMount() {
+    window.location.assign(
+      'https://www.unternehmertum.de/landingpages/digital-business-lab'
+    );
+  }
+
+  render() {
+    return <React.Fragment />;
+  }
+}
+
+export default DBL;
